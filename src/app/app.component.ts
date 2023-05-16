597c1e1fc24dad13937102546af52ce8bc98f00a
Sharath Chandra <sharathchandra0033@gmail.com>
2023-05-16 14:59:32 +0530

    File Explorer related changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,46 @@
 import { Component } from '@angular/core';
+import { NestedTreeControl } from '@angular/cdk/tree';
+import { MatTreeNestedDataSource } from '@angular/material/tree';
+
+/**
+ * File data with nested structure.
+ * Each node has a name and an optional list of children.
+ */
+interface FileNode {
+  name: string;
+  children?: FileNode[];
+}
+
+const TREE_DATA: FileNode[] = [{
+  name: 'Docs',
+  children: [{
+    name: 'Doc 1'
+  }, {
+    name: 'Doc 2'
+  }, {
+    name: 'Doc 3'
+  }],
+}, {
+  name: 'Images',
+  children: [{
+    name: 'Images Sub 1',
+    children: [{
+      name: 'Image 1'
+    }, {
+      name: 'Image 7'
+    }],
+  }, {
+    name: 'Images Sub 2',
+    children: [{
+      name: 'Image 12'
+    }, {
+      name: 'Image 26'
+    }],
+  }],
+}, {
+  name: 'Projects',
+  children: [],
+}];
 
 @Component({
   selector: 'app-root',
@@ -6,5 +48,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'file-explorer';
+  treeControl = new NestedTreeControl<FileNode>(node => node.children);
+  dataSource = new MatTreeNestedDataSource<FileNode>();
+
+  constructor() {
+    this.dataSource.data = TREE_DATA;
+  }
+
+  hasChild = (_: number, node: FileNode) => !!node.children && node.children.length > 0;
+
+  isFolder = (_: number, node: FileNode) => !!node.children && node.children.length >= 0;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatTreeModule } from '@angular/material/tree';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTreeModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   declarations: [
     AppComponent
-  ],
-  imports: [
-    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
